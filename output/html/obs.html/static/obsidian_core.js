9a85b70c779f0ba1d585e7ddfcc250d4b216e92b
Jay Colson <jay.colson@synchronoss.com>
2022-09-21 23:46:49 +0100

    some quick edits

--- output/html/obs.html/static/obsidian_core.js
+++ output/html/obs.html/static/obsidian_core.js
@@ -11,7 +11,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/GUS/output/html";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '299441887956957553485573086074980508625'                       // used to check whether the localStorage data is stale
+var gzip_hash = '272833727180927400682064306587806202215'                       // used to check whether the localStorage data is stale
 
 
 // Onloads
